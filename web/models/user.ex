c5c73eba060a64805e8c24417ad3dd6aff437147
Brad Leyden <bleyden92@gmail.com>
2018-08-22 15:36:01 -0400

    Initial commit
    Initial commit for Phoenix application, including basic authentication
    functionality using Guardian and Comeonin

---
+++ web/models/user.ex
@@ -0,0 +1,39 @@
+defmodule Todo.User do
+  use Todo.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, ~w(email), [])
+    |> validate_format(:email, ~r/@/)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash,
+                    Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
+  end
+end
---
+++ web/router.ex
@@ -0,0 +1,34 @@
+defmodule Todo.Router do
+  use Todo.Web, :router
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :browser_auth do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.EnsureAuthenticated, handler: Todo.Token
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Todo do
+    pipe_through :browser # Use the default browser stack
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:new, :create, :delete]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+  end
+
+  scope "/", Todo do
+    pipe_through [:browser, :browser_auth]
+    resources "/users", UserController, only: [:show, :index, :update]
+  end
+end
